2b276190552d9f8958915222f1c86db116a484fb
Prathipan <prathipan24p@gmail.com>
2022-09-09 08:16:19 +0530

    formik validation

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,64 +1,131 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { useNavigate } from 'react-router-dom';
 import { API } from './global';
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 function AddForm() {
     const navigate = useNavigate();
-    const [name, setName] = useState();
-  const [rating, setRating] = useState();
-  const [poster, setPoster] = useState();
-  const [summary, setSummary] = useState();
-  const [trailer, setTrailer] = useState();
+  //   const [name, setName] = useState();
+  // const [rating, setRating] = useState();
+  // const [poster, setPoster] = useState();
+  // const [summary, setSummary] = useState();
+  // const [trailer, setTrailer] = useState();
 
-    const handleSubmit = () => {
-        const newMovie = {
-          name: name,
-          rating: rating,
-          poster: poster,
-          summary: summary,
-          trailer: trailer
-        };
-        fetch(`${API}`,{
-          method : "POST",
-          body : JSON.stringify(newMovie),
-          headers : {
-            "Content-Type": "application/json"
-          }
-        }).then(() => navigate("/movies"));
-        //  console.log(movie);
-        // setAddMovie([...Addmovies, movie]);
-      };
+  const movieValidationSchema = yup.object({
+    name : yup.string().required("*Name field is mandatory").min(3),
+    rating: yup.number().required("*Rating is mandatory").min(1).max(10),
+    poster: yup.string().required("*Poster field is mandatory").min(5),
+    summary: yup.string().required("*Summary  is mandatory").min(5),
+    trailer : yup.string().required("*Trailer field is mandatory").min(5)
+  })
+
+
+  const formik = useFormik({
+    initialValues : {
+      name : "",
+      rating : "",
+      poster : "",
+      summary : "",
+      trailer : ""
+    },
+    validationSchema : movieValidationSchema ,
+    onSubmit : (newMovie) => {
+      // console.log("onSubmit : " , newMovie);
+      fetch(`${API}`,{
+              method : "POST",
+              body : JSON.stringify(newMovie),
+              headers : {
+                "Content-Type": "application/json"
+              }
+            }).then(() => navigate("/movies"));
+    }
+  })
+
+    // const addMovie = () => {
+    //     const newMovie = {
+    //       name: name,
+    //       rating: rating,
+    //       poster: poster,
+    //       summary: summary,
+    //       trailer: trailer
+    //     };
+    //     fetch(`${API}`,{
+    //       method : "POST",
+    //       body : JSON.stringify(newMovie),
+    //       headers : {
+    //         "Content-Type": "application/json"
+    //       }
+    //     }).then(() => navigate("/movies"));
+    //     //  console.log(movie);
+    //     // setAddMovie([...Addmovies, movie]);
+    //   };
   return (
     <>
-    <div className="formGroup">
+    <form onSubmit={formik.handleSubmit} className="formGroup">
         <TextField
           label="Enter the name"
           variant="outlined"
-          onChange={(e) => setName(e.target.value)}
+          id="name"
+          name="name"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error = {formik.touched.name && formik.errors.name }
+          helperText={formik.touched.name && formik.errors.name ? formik.errors.name : ""}
         />
+        {/* {formik.touched.name && formik.errors.name ? formik.errors.name : ""} */}
         <TextField
           label="Enter the rating"
           variant="outlined"
-          onChange={(e) => setRating(e.target.value)}
+          id="rating"
+          name="rating"
+          value={formik.values.rating}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error = {formik.touched.rating && formik.errors.rating }
+          helperText={formik.touched.rating && formik.errors.rating ? formik.errors.rating : ""}
         />
+        {/* {formik.touched.rating && formik.errors.rating ? formik.errors.rating : ""} */}
         <TextField
           label="Enter the poster link"
           variant="outlined"
-          onChange={(e) => setPoster(e.target.value)}
+          id="poster"
+          name="poster"
+          value={formik.values.poster}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error = {formik.touched.poster && formik.errors.poster}
+          helperText={formik.touched.poster && formik.errors.name ? formik.errors.poster : ""}
         />
+        {/* {formik.touched.poster && formik.errors.poster ? formik.errors.poster : ""} */}
         <TextField
           label="Enter the summary"
           variant="outlined"
-          onChange={(e) => setSummary(e.target.value)}
+          id="summary"
+          name="summary"
+          value={formik.values.summary}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error = {formik.touched.summary && formik.errors.summary}
+          helperText={formik.touched.summary && formik.errors.summary ? formik.errors.summary : ""}
         />
+        {/* {formik.touched.summary && formik.errors.summary ? formik.errors.summary : ""} */}
         <TextField
           label="Enter the trailer"
           variant="outlined"
-          onChange={(e) => setTrailer(e.target.value)}
+          id="trailer"
+          name="trailer"
+          value={formik.values.trailer}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error = {formik.touched.trailer && formik.errors.trailer}
+          helperText={formik.touched.trailer && formik.errors.trailer ? formik.errors.trailer : ""}
         />
-        <Button variant="contained" onClick={handleSubmit}>
+        {/* {formik.touched.trailer && formik.errors.trailer ? formik.errors.trailer : ""} */}
+        <Button variant="contained" type="submit">
           Add Movie
         </Button>
         {/*<input className="inputBox" type="text" placeholder="Enter the name" onChange={(e)=> setName(e.target.value)}/>
@@ -70,7 +137,7 @@ function AddForm() {
             Add Movie
           </button>
         </div>*/}
-      </div>
+      </form>
        </>
   )
 }
--- src/EditMovies.js
+++ src/EditMovies.js
@@ -5,6 +5,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import { API } from "./global";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 function EditForm() {
   const { id } = useParams();
@@ -36,69 +38,124 @@ function EditForm() {
 
 const EditFunction = ({ movie }) => {
   const navigate = useNavigate();
-  const [name, setName] = useState(movie.name);
-  const [rating, setRating] = useState(movie.rating);
-  const [poster, setPoster] = useState(movie.poster);
-  const [summary, setSummary] = useState(movie.summary);
-  const [trailer, setTrailer] = useState(movie.trailer);
+  // const [name, setName] = useState(movie.name);
+  // const [rating, setRating] = useState(movie.rating);
+  // const [poster, setPoster] = useState(movie.poster);
+  // const [summary, setSummary] = useState(movie.summary);
+  // const [trailer, setTrailer] = useState(movie.trailer);
 
-  const handleUpdate = () => {
-    const updatedMovie = {
-      name: name,
-      rating: rating,
-      poster: poster,
-      summary: summary,
-      trailer: trailer,
-    };
-    fetch(`${API}/${movie.id}`, {
-      method: "PUT",
-      body: JSON.stringify(updatedMovie),
-      headers: {
-        "content-type": "application/json",
-      },
-    }).then(navigate("/movies"));
+  const movieValidationSchema = yup.object({
+    name : yup.string().required("*Name field is mandatory").min(3),
+    rating: yup.number().required("*Rating is mandatory").min(1).max(10),
+    poster: yup.string().required("*Poster field is mandatory").min(5),
+    summary: yup.string().required("*Summary  is mandatory").min(5),
+    trailer : yup.string().required("*Trailer field is mandatory").min(5)
+  })
+
+  const formik = useFormik({
+    initialValues : {
+      name : movie.name,
+      rating : movie.rating,
+      poster : movie.poster,
+      summary : movie.summary,
+      trailer : movie.trailer
+    },
+    validationSchema : movieValidationSchema ,
+    onSubmit : (updatedMovie) => {
+      // console.log("onSubmit : " , newMovie);
+      fetch(`${API}/${movie.id}`, {
+            method: "PUT",
+            body: JSON.stringify(updatedMovie),
+            headers: {
+              "content-type": "application/json",
+            },
+          }).then(navigate("/movies"));
+    }
+  })
+
+
+  // const handleUpdate = () => {
+  //   const updatedMovie = {
+  //     name: name,
+  //     rating: rating,
+  //     poster: poster,
+  //     summary: summary,
+  //     trailer: trailer,
+  //   };
+  //   fetch(`${API}/${movie.id}`, {
+  //     method: "PUT",
+  //     body: JSON.stringify(updatedMovie),
+  //     headers: {
+  //       "content-type": "application/json",
+  //     },
+  //   }).then(navigate("/movies"));
     // const copyMovie = [...Addmovies];
     // copyMovie[id] = updatedMovie;
     //  console.log("Edited movie : " ,copyMovie[id]);
     // setAddMovie(copyMovie);
-  };
+  // };
 
   return (
-    <div className="formGroup">
+    <form className="formGroup" onSubmit={formik.handleSubmit}>
       <TextField
-        value={name}
+        value={formik.values.name}
         label="Enter the name"
         variant="outlined"
-        onChange={(e) => setName(e.target.value)}
+        id="name"
+        name="name"
+        onChange={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.name && formik.errors.name }
+        helperText={formik.touched.name && formik.errors.name ? formik.errors.name : ""}
       />
       <TextField
-        value={rating}
+        value={formik.values.rating}
         label="Enter the rating"
         variant="outlined"
-        onChange={(e) => setRating(e.target.value)}
+        id="rating"
+        name="rating"
+        onChange={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.rating && formik.errors.rating }
+        helperText={formik.touched.rating && formik.errors.rating ? formik.errors.rating : ""}
       />
       <TextField
-        value={poster}
+        value={formik.values.poster}
         label="Enter the poster link"
         variant="outlined"
-        onChange={(e) => setPoster(e.target.value)}
+        id="poster"
+        name="poster"
+        onChange={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.poster && formik.errors.poster }
+        helperText={formik.touched.poster && formik.errors.poster ? formik.errors.poster : ""}
       />
       <TextField
-        value={summary}
+        value={formik.values.summary}
         label="Enter the summary"
         variant="outlined"
-        onChange={(e) => setSummary(e.target.value)}
+        id="summary"
+        name="summary"
+        onChange={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.summary && formik.errors.summary }
+        helperText={formik.touched.summary && formik.errors.summary ? formik.errors.summary : ""}
       />
       <TextField
-        value={trailer}
+        value={formik.values.trailer}
         label="Enter the trailer"
         variant="outlined"
-        onChange={(e) => setTrailer(e.target.value)}
+        id="trailer"
+        name="trailer"
+        onChange={formik.handleChange}
+        onBlur = {formik.handleBlur}
+        error = {formik.touched.trailer && formik.errors.trailer }
+          helperText={formik.touched.trailer && formik.errors.trailer ? formik.errors.trailer : ""}
       />
-      <Button variant="contained" onClick={handleUpdate}>
+      <Button variant="contained" type="submit">
         Update
       </Button>
-    </div>
+    </form>
   );
 };
 
