a63988a63f2f5845bdeaa2e8948242f54a0d017f
Prathipan <prathipan24p@gmail.com>
2022-09-29 23:25:34 +0530

    extra card

--- src/App.js
+++ src/App.js
@@ -93,34 +93,39 @@ function Msg() {
       container
       spacing={2}
       direction="column"
-      alignItems="left"
+      alignItems="center"
       justify="center"
       style={{ minHeight: "100vh" }}
     >
       <Grid item xs={3}>
-        <div>
+        <div className="container">
           <h1>Welcome to Movies App 😁🎉</h1>
-          <h2>All Movies Section</h2>
-          <ul>
-            <li>
-              We can have list of movies with the description with IMDB rating .
-            </li>
-            <li> i button shows the trailer and description of the movie.</li>
-            <li>We can like and dislike the movies here.</li>
-            <li>
-              If we found some typos , we can correct by clicking the pen icon
-            </li>
-            <li>If we don't want that movie we can delete that</li>
-          </ul>
-          <h2>Add Movies Section</h2>
-          <ul>
-            <li>If we want to add movies to the list we can add them</li>
-            <li>
-              We need to enter all details over there , If not it will show
-              error
-            </li>
-            <li>Rating must be 0-10</li>
-          </ul>
+          <div className="card">
+            <h2>All Movies Section</h2>
+            <ul>
+              <li>
+                We can have list of movies with the description with IMDB rating
+                .
+              </li>
+              <li> i button shows the trailer and description of the movie.</li>
+              <li>We can like and dislike the movies here.</li>
+              <li>
+                If we found some typos , we can correct by clicking the pen icon
+              </li>
+              <li>If we don't want that movie we can delete that</li>
+            </ul>
+          </div>
+          <div className="card">
+            <h2>Add Movies Section</h2>
+            <ul>
+              <li>If we want to add movies to the list we can add them</li>
+              <li>
+                We need to enter all details over there , If not it will show
+                error
+              </li>
+              <li>Rating must be 0-10</li>
+            </ul>
+          </div>
         </div>
       </Grid>
     </Grid>
